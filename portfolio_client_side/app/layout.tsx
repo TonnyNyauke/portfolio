9d5aca84bf16dda0e10e2d5b66c42fbd6bc43d2d
TonnyNyauke <nyaukeindustries@gmail.com>
2025-11-07 17:15:30 +0300

    wueh

--- portfolio_client_side/app/layout.tsx
+++ portfolio_client_side/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -196,7 +197,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         <Header />
+        <Analytics />
         <main>{children}</main>
         <Footer />
       </body>
